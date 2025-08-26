ff0a6111395c3c924759ada1477543fba5e0a37f
Arole Josia <aroletella@gmail.com>
2025-08-26 12:54:01 -0400

    Update iOS project settings, icons and launch screen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,12 @@
 import Flutter
 import UIKit
-import UserNotifications // Importez ceci
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-   if #available(iOS 10.0, *) {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
-          if granted {
-            print("Permissions de notification accordées.")
-          } else {
-            print("Permissions de notification refusées.")
-          }
-          }}
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
